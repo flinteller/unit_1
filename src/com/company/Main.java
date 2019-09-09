a39e9498f7a5f699bdb7d843b26ce169ac49f03c
flinteller <flinteller@ssfs.org>
2019-09-09 14:05:03 -0400

    Practiced Escape characters

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,8 @@ public class Main {
 	// write your code here
         System.out.println("Hello, World!");
         System.out.println("This is another line");
+
+        System.out.println("This is \"Flint's\" computer");
+        System.out.println("I am splitting this \n over two lines");
     }
 }
