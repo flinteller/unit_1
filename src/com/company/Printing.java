50bcfe6bc5180b16353626d0fb36bd0a3df72bd2
flinteller <flinteller@ssfs.org>
2019-09-10 15:03:34 -0400

    Printed basic statements using multiple lines

--- src/com/company/Printing.java
+++ src/com/company/Printing.java
@@ -2,6 +2,13 @@ package com.company;
 
 public class Printing {
     public static void main(String[] args) {
+        System.out.println("02:33");
+        System.out.println("09/10/2019");
+        System.out.println("It is a great \ntime to be a \ncomputer programmer!");
+        System.out.print("It is a great time to be a ");
+        System.out.print("computer ");
+        System.out.print("programmer!");
+
 
     }
 }
