b2504b3d0bc9ce333b2be0cd0e5ee77257238489
flinteller <flinteller@ssfs.org>
2019-09-10 15:04:35 -0400

    Used lots and lots of escape characters

---
+++ src/com/company/Escape.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Escape {
+    public static void main(String[] args) {
+        System.out.println("What is the difference between");
+        System.out.println("a ' and a \"? Or between a \" and a \\\"? ");
+        System.out.println("");
+        System.out.println("One is what we see when we're typing our program. \nThe other is what appears on the \"console.\"");
+
+        System.out.println("  \\/ \n \\\\// \n\\\\\\///\n///\\\\\\ \n //\\\\");
+        System.out.print("  /\\");
+
+    }
+}
