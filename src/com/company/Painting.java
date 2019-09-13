79763ecaf28a3ecae8e5da6e5c3db45497da8946
flinteller <flinteller@ssfs.org>
2019-09-13 10:16:10 -0400

    Got user input to paint a room with no windows or doors

--- src/com/company/Painting.java
+++ src/com/company/Painting.java
@@ -1,18 +1,29 @@
 package com.company;
+import java.util.Scanner;
 
 public class Painting {
     public static void main(String[] args) {
+
+        double length;
+        double width;
+        double height;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the length of you room in feet:");
+        length = scan.nextDouble();
+
+        System.out.println("Please enter the height of you room in feet:");
+        height = scan.nextDouble();
+
+        System.out.println("Please enter the width of you room in feet:");
+        width = scan.nextDouble();
+
         final int paint_buckets = 350;
-        int length = 32;
-        int width = 40;
-        int height = 16;
-        int door = 20;
-        int window = 15;
 
-        int ceiling_area = length *width;
-        int total_wall_area = (((width * height) * 2) + ((length * height) * 2)) - ((door * 2) + (window * 4));
+        double ceiling_area = length * width;
+        double total_wall_area = ((width * height) * 2) + ((length * height) * 2);
 
-        int room_area = ceiling_area + total_wall_area;
+        double room_area = ceiling_area + total_wall_area;
         double buckets_needed = room_area / paint_buckets;
         System.out.println("You should buy " + buckets_needed + " buckets of paint for a room area of " + room_area + " square feet.");
 
