c2665a3366b5ac6981a044e0edd652b093487dae
flinteller <flinteller@ssfs.org>
2019-09-16 10:48:56 -0400

    Converted 115 pounds to kilograms

---
+++ src/com/company/PoundsToKilos.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class PoundsToKilos {
+    public static void main(String[] args) {
+        double lbs;
+        double kg;
+
+        lbs = 115;
+        kg = lbs / 2.205;
+        System.out.println(lbs + " pounds = " + kg + " kilograms");
+    }
+}
