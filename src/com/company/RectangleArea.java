d1da1e2b25d40cfab48205388c5fc0d2c47a99a5
flinteller <flinteller@ssfs.org>
2019-09-17 12:40:35 -0400

    Got length and width of user's rectangle and found area

--- src/com/company/RectangleArea.java
+++ src/com/company/RectangleArea.java
@@ -1,9 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class RectangleArea {
     public static void main(String[] args) {
-        int length = 14;
-        int width = 6;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the length: ");
+        int length = scan.nextInt();
+        System.out.println("Please enter the width: ");
+        int width = scan.nextInt();
         int area = length * width;
         System.out.println("Area: " + area);
     }
