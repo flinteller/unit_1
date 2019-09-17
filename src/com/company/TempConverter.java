43be42dbcc39cc31e666f139dc5d5f6aeb3b309d
flinteller <flinteller@ssfs.org>
2019-09-17 12:29:28 -0400

    Converted user's input temp from F to C

--- src/com/company/TempConverter.java
+++ src/com/company/TempConverter.java
@@ -1,8 +1,11 @@
 package com.company;
+import java.util.Scanner;
 
 public class TempConverter {
     public static void main(String[] args) {
-        int temp_f = 78;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the temp in fahrenheit: ");
+        int temp_f = scan.nextInt();
         int temp_c = (temp_f - 32) * 5 / 9;
 
         System.out.println(temp_f + " fahrenheit is equal to " + temp_c + " celsius");
