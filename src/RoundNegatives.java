c0e46ec54623e9be081ec63125ed9ad2e4674cb0
flinteller <flinteller@ssfs.org>
2019-09-19 14:49:43 -0400

    Rounded negative number and ADDED MISSING BRACKET

--- src/RoundNegatives.java
+++ src/RoundNegatives.java
@@ -8,7 +8,8 @@ public class RoundNegatives {
         double user_num;
         System.out.println("Enter a negative decimal number: ");
         user_num = scan.nextDouble();
-        int rounded_num = (int)(user_num - 0.5);
+        int rounded_num = (int) (user_num - 0.5);
         System.out.println(user_num + "rounded to a whole number is: " + rounded_num);
+
     }
 }
